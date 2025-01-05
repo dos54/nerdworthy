e723546a8949c0019cbc291a7b656d59c4f17649
Steven Thomas <stevencthomas54@gmail.com>
2025-01-04 20:49:47 -0900

    More database integration

--- src/utils/getEvents.ts
+++ src/utils/getEvents.ts
@@ -1,11 +1,23 @@
 import {neon} from '@neondatabase/serverless';
 
+type Event = {
+    title: string;
+    image: string;
+    description: string;
+}
+
 export async function getEvents() {
     'use server';
 
     const sql = neon(`${process.env.DATABASE_URL}`);
 
-    const events = await sql('SELECT title, image, description FROM events');
+    const rawEvents = await sql('SELECT title, image, description FROM events');
+
+    const events: Event[] = rawEvents.map((event) => ({
+        title: event.title,
+        image: event.image,
+        description: event.description,
+    }));
 
     return events;
 }
