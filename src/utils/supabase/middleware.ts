645fe3348d6edf0a27d28fba9143385fa5442662
Steven Thomas <stevencthomas54@gmail.com>
2025-01-05 18:30:36 -0900

    I am literally crying rn please help :cri: :cri:

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -1,5 +1,4 @@
 import { createServerClient } from "@supabase/ssr";
-import { create } from "domain";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function updateSession(request: NextRequest) {
@@ -16,7 +15,7 @@ export async function updateSession(request: NextRequest) {
                     return request.cookies.getAll();
                 },
                 setAll(cookiesToSet) {
-                    cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value))
+                    cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value))
                     supabaseResponse = NextResponse.next({
                         request,
                     });
